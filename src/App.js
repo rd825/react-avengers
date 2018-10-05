34717989eb853e5685860506b54203b8c8ce233e
Rahul Desai <rd825@georgetown.edu>
2018-10-05 19:02:02 -0400

    Avengers pre-class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      avengers: [
+        'Iron Man', 
+        'Captain America', 
+        'Thor', 
+        'The Hulk', 
+        'Black Widow', 
+        'Hawkeye', 
+        'Black Panther',
+        'Spider-Man',
+        'Doctor Strange',
+        'Ant-Man',
+        'War Machine',
+        'Falcon',
+        'Vision',
+        'Scarlet Witch',
+        'The Winter Soldier',
+        'Star Lord',
+        'Gamora',
+        'Drax the Destroyer',
+        'Rocket Raccoon',
+        'Groot',
+        'Mantis',
+      ],
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Avengers: by Rahul Desai</h1>
+        {this.state.avengers.map((avenger, i) => <p key={i}>{avenger}</p>)}
       </div>
     );
   }
